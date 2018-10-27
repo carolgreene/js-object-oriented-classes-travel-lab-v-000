31efbc86c50732f66d4924e222a960048b8fada8
Carol Greene <carolgreene3@gmail.com>
2018-10-27 21:47:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,10 +31,9 @@ class Route {
 
   estimatedTime(peak) {
     if (peak) {
-      return blocksTravelled() / 2
+      return this.blocksTravelled() / 2
     } else {
-      return blocksTravelled() / 3
+      return this.blocksTravelled() / 3
     }
     }
   }
-
