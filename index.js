3e331b891ff288ba08e0798d38d1a3dfaaf52e62
Carol Greene <carolgreene3@gmail.com>
2018-10-27 21:21:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class Driver {
   yearsExperienceFromBeginningOf(year) {
     let endDate = new Date(year, 0, 1)
     console.log(endDate)
-    let totalYears = endDate - this.startDate /
+    let totalYears = (endDate - this.startDate) /
     (365 * 24 * 60 * 60 * 1000)
     console.log(totalYears)
   }
