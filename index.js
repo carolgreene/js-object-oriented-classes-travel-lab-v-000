ebed1ec58b643a77f292778ba216e1ff9318f23f
Carol Greene <carolgreene3@gmail.com>
2018-10-27 21:33:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ class Driver {
   }
 }
 
+let eastWest = ['1st Avenue','2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+
 class Route {
   constructor(beginningLocation, endingLocation) {
     this.beginningLocation = beginningLocation
     this.endingLocation = endingLocation
   }
 
-let eastWest = ['1st Avenue','2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
   blocksTravelled() {
     let horizontalDistance = eastWest.indexOf(endingLocation) - eastWest.indexOf(beginningLocation)
